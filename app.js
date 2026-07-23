d486492e6f6e7f41555151231a99befbcf46a800
Elielton Integrada Robo-whatsapp <integradaneuropsicologia@gmail.com>
2026-07-23 18:51:22 -0300

    feat: add PHQ-9 to patient area

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ const TEST_URLS = {
   IDATE_V2: "https://integradaneuropsicologia.github.io/formularios/IDATE_V2/",
   GDS_V2: "https://integradaneuropsicologia.github.io/formularios/GDS_V2/",
   PSS_V2: "https://integradaneuropsicologia.github.io/formularios/PSS_V2/",
+  PHQ9_V2: "https://integradaneuropsicologia.github.io/formularios/PHQ9_V2/",
   TORRE_DE_LONDRES_V2: "https://integradaneuropsicologia.github.io/formularios/TORRE_DE_LONDRES_V2/",
   IDADI_V2: "https://integradaneuropsicologia.github.io/formularios/IDADI_V2/",
   SRS2_AUTORRELATO: "https://integradaneuropsicologia.github.io/srs2/",
@@ -79,6 +80,7 @@ const FORM_CACHE_VERSIONS = {
   IDATE_V2: "839367c",
   GDS_V2: "edc0bbd",
   PSS_V2: "3d9eab8",
+  PHQ9_V2: "7ecfb1c",
   TORRE_DE_LONDRES_V2: "06fab5e",
   IDADI_V2: "a81eda5",
   PFISTER_V2: "0ade0bc",
@@ -460,6 +462,13 @@ const FALLBACK_TEST_META = {
     shareable: false,
     order: 19,
     form_url: TEST_URLS.PSS_V2
+  },
+  PHQ9_V2: {
+    label: "Saúde do Paciente (PHQ-9)",
+    source: "paciente",
+    shareable: false,
+    order: 21,
+    form_url: TEST_URLS.PHQ9_V2
   },
   TORRE_DE_LONDRES_V2: {
     label: "Torre de Londres",
